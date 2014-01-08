eaad17fdfdcb3696d09f858bef93f9b02b762b56
tb3 <tb3@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-08 02:23:25 +0000

    Adding first version of the plugin
    git-svn-id: https://plugins.svn.wordpress.org/facebook-popup-box/trunk@834675 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ facebookpopup_footer.php
@@ -0,0 +1,31 @@
+<?php
+/*
+ * Its illegal to remove ads without prior written permission from Vikas Singhal (contact@tb3.co.in)
+ * Feel free to change the appId
+ */
+?>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=669871346370393";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+<div id='facebookpopup-fanback'>
+<div id='facebookpopup-fan-exit'>
+</div>
+<div id='facebookpopup-fanbox'>
+<div id='facebookpopup-fanclose'>
+</div>
+<div class='facebookpopup-remove-border'>
+</div>
+<script> facebookpopup_delay = __DELAY__;</script>
+
+
+<div style='overflow: hidden; margin-top: -19px; width: 402px; height: 230px;' class="fb-like-box" 
+     data-href="http://www.facebook.com/__URL__" data-width="402" data-height="255" data-colorscheme="light" data-show-faces="true" data-header="false" data-stream="false" data-show-border="false"></div>
+<center>
+<span id="facebookpopup-linkit"><a href="https://www.xtendify.com/en/product/83-Facebook_Popup_Like_Box_for_WordPress">Get Widget</a></span></center>
+</div>
+</div>
