85596b388181f2403ec8435e4057890407754ba3
tb3 <tb3@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-09-28 17:07:57 +0000

    removed backlink and removed malicious adnetwork - 1.5
    git-svn-id: https://plugins.svn.wordpress.org/facebook-popup-box/tags/1.5@1255236 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ facebookpopup_footer.php
@@ -0,0 +1,53 @@
+<?php 
+/*
+ * Its illegal to remove ads without prior written permission from the developer (contact [at] tb3.co.in)
+ * Feel free to change the appId
+ */
+?>
+<?php 
+$fb_footer = '
+<div id="fb-root"></div><div id="facebookpopup-fanback">';
+if(!isset($_COOKIE['popup_user_login']))  {
+	$fb_footer .= '
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=669871346370393";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, "script", "facebook-jssdk"));</script>
+
+<div id="facebookpopup-fan-exit">
+</div>
+<div id="facebookpopup-fanbox">
+<div id="facebookpopup-fanclose">
+</div>
+<div class="facebookpopup-remove-border">
+</div>
+<script> facebookpopup_delay = __DELAY__;</script>
+
+
+<div style="overflow: hidden; margin-top: -19px; width: 402px; height: 230px;" class="fb-like-box" 
+     data-href="http://www.facebook.com/__URL__" data-width="402" data-height="255" data-colorscheme="light" data-show-faces="true" data-header="false" data-stream="false" data-show-border="false"></div>
+<center>
+<span id="facebookpopup-linkit">	
+<script src="//ads.lfstmedia.com/getad?site=230633" type="text/javascript"></script>
+	
+<script type="text/javascript">
+    //<![CDATA[
+        LSM_Slot({
+            adkey: \'c8f\',
+            ad_size: \'320x50\',
+            slot: \'slot90105\'
+        });
+    //]]>
+</script>
+<br/>
+
+
+ '; } 
+  $fb_footer .= ' 
+</span></center>
+</div>
+</div>
+ '; ?>
